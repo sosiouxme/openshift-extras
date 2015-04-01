3bb21d6f4cc1c14dd601e4dc9f4f330638422359
Luke Meyer <lmeyer@redhat.com>
2015-04-01 17:44:10 -0400

    client: nodes that are schedulable but not ready

--- diagnostics/client/client.go
+++ diagnostics/client/client.go
@@ -4,11 +4,70 @@ import (
 	"fmt"
 	kapi "github.com/GoogleCloudPlatform/kubernetes/pkg/api"
 	clientcmdapi "github.com/GoogleCloudPlatform/kubernetes/pkg/client/clientcmd/api"
+	//"github.com/kr/pretty"
 	"github.com/openshift/openshift-extras/diagnostics/log"
 	"github.com/openshift/openshift-extras/diagnostics/types"
 )
 
 var Diagnostics = map[string]types.Diagnostic{
+	"NodeDefinitions": types.Diagnostic{
+		Description: "Check node records on master",
+		Condition: func(env *types.Environment) (skip bool, reason string) {
+			if env.ClusterAdminFactory == nil {
+				return true, "Client does not have cluster-admin access and cannot see node records"
+			}
+			return false, ""
+		},
+		Run: func(env *types.Environment) {
+			var err error
+			var nodes *kapi.NodeList
+			if _, kclient, err := env.ClusterAdminFactory.Clients(); err == nil {
+				nodes, err = kclient.Nodes().List()
+			}
+			if err != nil {
+				log.Errorf("clGetNodesFailed", `
+Client error while retrieving node records. Client retrieved records
+during discovery, so this is likely to be a transient error. Try running
+diagnostics again. If this message persists, there may be a permissions
+problem with getting node records. The error was:
+
+(%T) %[1]v`, err)
+				return
+			}
+			for _, node := range nodes.Items {
+				//pretty.Println("Node record:", node)
+				var ready, schedulable *kapi.NodeCondition
+				for i, condition := range node.Status.Conditions {
+					switch condition.Type {
+					case kapi.NodeReady:
+						ready = &node.Status.Conditions[i]
+					case kapi.NodeSchedulable:
+						schedulable = &node.Status.Conditions[i]
+					}
+				}
+				//pretty.Println("Node conditions for "+node.Name, ready, schedulable)
+				if schedulable != nil && schedulable.Status == kapi.ConditionFull && (ready == nil || ready.Status != kapi.ConditionFull) {
+					msg := log.Msg{
+						"node": node.Name,
+						"tmpl": `
+Node {{.node}} is allowed to have pods scheduled but is not ready to
+run them. Ready status is {{.status}} because "{{.reason}}"
+
+While in this state, pods could be scheduled to deploy on the node but
+will not be deployed until the node achieves readiness.`,
+					}
+					if ready == nil {
+						msg["status"] = "None"
+						msg["reason"] = "There is no readiness record."
+					} else {
+						msg["status"] = ready.Status
+						msg["reason"] = ready.Reason
+					}
+					log.Errorm("clNodeBroken", msg)
+				}
+			}
+		},
+	},
 	"ConfigContexts": types.Diagnostic{
 		Description: "Test that client config contexts have no undefined references",
 		Condition: func(env *types.Environment) (skip bool, reason string) {
