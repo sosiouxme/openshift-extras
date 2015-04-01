d48f99783c4372077788cd5a88ed6d407742b794
Luke Meyer <lmeyer@redhat.com>
2015-04-01 16:25:40 -0400

    log: improve whitespace separation

--- diagnostics/log/text.go
+++ diagnostics/log/text.go
@@ -9,7 +9,8 @@ import (
 	"strings"
 )
 
-var ttyOutput bool = true
+var ttyOutput bool = true   // don't output colors to non-tty
+var lastNewline bool = true // keep track of newline separation
 
 func init() {
 	if runtime.GOOS == "linux" && !terminal.IsTerminal(int(os.Stdout.Fd())) {
@@ -24,7 +25,17 @@ func (t *textLogger) Write(l Level, msg Msg) {
 	if ttyOutput {
 		ct.ChangeColor(l.Color, l.Bright, ct.None, false)
 	}
-	fmt.Println(l.Prefix + strings.Replace(fmt.Sprintf("%v", msg["text"]), "\n", "\n       ", -1))
+	text := strings.TrimSpace(fmt.Sprintf("%v", msg["text"]))
+	if strings.Contains(text, "\n") { // separate multiline comments with newlines
+		if !lastNewline {
+			fmt.Println() // separate from previous one-line log msg
+		}
+		text = text + "\n"
+		lastNewline = true
+	} else {
+		lastNewline = false
+	}
+	fmt.Println(l.Prefix + strings.Replace(text, "\n", "\n       ", -1))
 	if ttyOutput {
 		ct.ResetColor()
 	}
