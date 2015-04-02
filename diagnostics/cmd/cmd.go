5ef949652fb54a0dcdc39bf3c440fc096e1d4a35
Luke Meyer <lmeyer@redhat.com>
2015-04-02 01:05:41 -0400

    improve diagnostic runner
    1. Allow -d flag for running specific diagnostic(s)
    2. Recover from diagnostic panics

--- diagnostics/cmd/cmd.go
+++ diagnostics/cmd/cmd.go
@@ -2,8 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-
 	"github.com/openshift/openshift-extras/diagnostics/client"
 	"github.com/openshift/openshift-extras/diagnostics/cmd/flags"
 	"github.com/openshift/openshift-extras/diagnostics/discovery"
@@ -12,6 +10,8 @@ import (
 	"github.com/openshift/openshift-extras/diagnostics/types"
 	osclientcmd "github.com/openshift/origin/pkg/cmd/util/clientcmd"
 	"github.com/spf13/cobra"
+	"os"
+	"strings"
 )
 
 const longDescription = `
@@ -36,7 +36,8 @@ func NewCommand() *cobra.Command {
 	factory := osclientcmd.New(osFlags) // side effect: add standard flags for openshift client
 
 	// Add flags separately from those inherited from the client
-	diagFlags := &flags.Flags{OpenshiftFlags: osFlags}
+	diagFlags := &flags.Flags{OpenshiftFlags: osFlags, Diagnostics: make(flags.List, 0)}
+	cmd.Flags().VarP(&diagFlags.Diagnostics, "diagnostics", "d", `comma-separated list of diagnostic names to run, e.g. "systemd.AnalyzeLogs"`)
 	cmd.Flags().IntVarP(&diagFlags.LogLevel, "loglevel", "l", 2, "Level of output: 0 = Error, 1 = Warn, 2 = Info, 3 = Debug")
 	cmd.Flags().StringVarP(&diagFlags.Format, "output", "o", "text", "Output format: text|json|yaml")
 	cmd.Flags().StringVarP(&diagFlags.OpenshiftPath, "openshift", "", "", "Path to 'openshift' binary")
@@ -48,7 +49,7 @@ func NewCommand() *cobra.Command {
 		c.SetOutput(os.Stdout)             // TODO: does this matter?
 		log.SetLogFormat(diagFlags.Format) // note, ignore the error returned if format is unknown, just do text
 		env := discovery.Run(diagFlags, factory)
-		Diagnose(env)
+		Diagnose(env, args)
 		log.Summary()
 		log.Finish()
 	}
@@ -67,28 +68,55 @@ func newVersionCommand(name string) *cobra.Command {
 	}
 }
 
-func Diagnose(env *types.Environment) {
+func Diagnose(env *types.Environment, args []string) {
 	allDiags := map[string]map[string]types.Diagnostic{"client": client.Diagnostics, "systemd": systemd.Diagnostics}
-	// TODO: run all of these in parallel but ensure sane output
-	// TODO: recover from diagnostics that panic so others can still run
-	// TODO: just run a specific (set of) diagnostic(s)
-	for area, diagnostics := range allDiags {
-		for name, d := range diagnostics {
-			if d.Condition != nil {
-				if skip, reason := d.Condition(env); skip {
-					if reason == "" {
-						log.Noticem("diagSkip", log.Msg{"area": area, "name": name, "diag": d.Description,
-							"tmpl": "Skipping diagnostic: {{.area}}.{{.name}}\nDescription: {{.diag}}"})
-					} else {
-						log.Noticem("diagSkip", log.Msg{"area": area, "name": name, "diag": d.Description, "reason": reason,
-							"tmpl": "Skipping diagnostic: {{.area}}.{{.name}}\nDescription: {{.diag}}\nBecause: {{.reason}}"})
-					}
-					continue
-				}
+	if list := env.Flags.Diagnostics; len(list) > 0 {
+		// just run a specific (set of) diagnostic(s)
+		for _, arg := range list {
+			parts := strings.SplitN(arg, ".", 2)
+			if len(parts) < 2 {
+				log.Noticef("noDiag", `There is no such diagnostic "%s"`, arg)
+				continue
 			}
-			log.Noticem("diagRun", log.Msg{"area": area, "name": name, "diag": d.Description,
-				"tmpl": "Running diagnostic: {{.area}}.{{.name}}\nDescription: {{.diag}}"})
-			d.Run(env)
+			area, name := parts[0], parts[1]
+			if diagnostics, exists := allDiags[area]; !exists {
+				log.Noticef("noDiag", `There is no such diagnostic "%s"`, arg)
+			} else if diag, exists := diagnostics[name]; !exists {
+				log.Noticef("noDiag", `There is no such diagnostic "%s"`, arg)
+			} else {
+				RunDiagnostic(area, name, diag, env)
+			}
+		}
+	} else {
+		// TODO: run all of these in parallel but ensure sane output
+		for area, diagnostics := range allDiags {
+			for name, diag := range diagnostics {
+				RunDiagnostic(area, name, diag, env)
+			}
 		}
 	}
+}
+
+func RunDiagnostic(area string, name string, diag types.Diagnostic, env *types.Environment) {
+	defer func() {
+		// recover from diagnostics that panic so others can still run
+		if r := recover(); r != nil {
+			log.Errorf("diagPanic", "Diagnostic '%s' crashed; this is usually a bug in either diagnostics or OpenShift. Stack trace:\n%+v", name, r)
+		}
+	}()
+	if diag.Condition != nil {
+		if skip, reason := diag.Condition(env); skip {
+			if reason == "" {
+				log.Noticem("diagSkip", log.Msg{"area": area, "name": name, "diag": diag.Description,
+					"tmpl": "Skipping diagnostic: {{.area}}.{{.name}}\nDescription: {{.diag}}"})
+			} else {
+				log.Noticem("diagSkip", log.Msg{"area": area, "name": name, "diag": diag.Description, "reason": reason,
+					"tmpl": "Skipping diagnostic: {{.area}}.{{.name}}\nDescription: {{.diag}}\nBecause: {{.reason}}"})
+			}
+			return
+		}
+	}
+	log.Noticem("diagRun", log.Msg{"area": area, "name": name, "diag": diag.Description,
+		"tmpl": "Running diagnostic: {{.area}}.{{.name}}\nDescription: {{.diag}}"})
+	diag.Run(env)
 }
--- diagnostics/cmd/flags/flags.go
+++ diagnostics/cmd/flags/flags.go
@@ -2,13 +2,27 @@ package flags
 
 import (
 	flag "github.com/spf13/pflag"
+	"strings"
 )
 
 type Flags struct {
+	Diagnostics    List
 	LogLevel       int
 	OpenshiftPath  string
 	OscPath        string
-	OpenshiftFlags *flag.FlagSet
 	Format         string
-	//	KubeconfigPath string
+	OpenshiftFlags *flag.FlagSet
+}
+
+type List []string
+
+func (l *List) Set(arg string) error {
+	*l = strings.Split(arg, ",")
+	return nil
+}
+func (l *List) Type() string {
+	return "list"
+}
+func (l *List) String() string {
+	return strings.Join(*l, ",")
 }
